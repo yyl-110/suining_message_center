a010d972d23a89d0bd508181a1ecd247fd610207
yyl <1040562591@qq.com>
2021-08-27 14:44:08 +0800

    feat:init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Table border :options="options" :tableSearch="tableSearch" :columns="columns" :operates="operates(this)" :isAdd="true" :dataApi="dataApi" :refresh="refresh" @addData="$router.push('/configure/spec/editspec')">
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Table from "@/components/Table";
+/* 设置表头 */
+const columns = [
+  {
+    prop: "name",
+    label: "工单编号",
+    align: "center",
+    fixed: true,
+  },
+  {
+    prop: "sortOrder",
+    align: "center",
+    label: "排序",
+  },
+];
+
+/* 操作按钮 */
+const operates = (that) => [
+  {
+    label: "编辑",
+    size: "mini",
+    type: "primary",
+    method: (row) => {
+      that.editData(row);
+    },
+  },
+  {
+    label: "删除",
+    type: "warning",
+    size: "mini",
+    setStyle: {
+      marginLeft: "10px",
+    },
+    method: (row) => {
+      that
+        .$confirm("确定要删除选中的记录？", "", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        })
+        .then(() => {
+          that.delSpec(row);
+        })
+        .catch(() => {});
+    },
+  },
+];
+
+// 搜索设置
+const tableSearch = [
+  {
+    label: "规格名称",
+    value: "name",
+    rulesLength: true,
+    labelProps: {
+      "label-width": "130",
+    },
+  },
+];
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      options: {},
+      columns,
+      operates,
+      tableSearch,
+      dataApi: "",
+      refresh: new Date().getTime(),
+    };
   },
 };
 </script>
